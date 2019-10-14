a494e87769aaa96f3058b32b7fc8ed822ca04027
Md. Habibur Rahman <habibcsepust@gmail.com>
2019-10-15 00:06:37 +0600

    Added

---
+++ BankTransactionUI/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTransactionUI
+{
+    public class Account
+    {
+        public string accountNumber;
+        public string customerName;
+        public double balance;
+
+        public void CreateAccount(string aNo,string cName)
+        {
+            accountNumber = aNo;
+            customerName = cName;
+        }
+
+        public double Deposit(double balnce)
+        {
+            balance = balance + balnce;
+            return balance;
+        }
+
+        public double Widthdraw(double balnce)
+        {
+            balance = balance - balnce;
+            return balance;
+        }
+    }
+}
